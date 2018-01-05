8e109dbb3af7724beadc577a446a36f5f87924bb
Vladimir Ivanov <zlumyo@gmail.com>
2018-01-05 12:01:57 +0300

    Added test to check players' limit

--- SaboteurTest/SaboteurNewGameTest.cs
+++ SaboteurTest/SaboteurNewGameTest.cs
@@ -1,19 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SaboteurFoundation;
+using System;
 
 namespace SaboteurTest
 {
     [TestClass]
     public class SaboteurNewGameTest
     {
-        private readonly string[] _minPlayers = { "player1", "player2", "player3" };
-        private readonly string[] _maxPlayers = { "player4", "player5", "player6",
+        private static readonly string[] _minPlayers = { "player1", "player2", "player3" };
+        private static readonly string[] _maxPlayers = { "player4", "player5", "player6",
                                                   "player7", "player8", "player9",
                                                   "player10" };
-        private readonly string[] _tooLessPlayers = { "player1", "player2" };
-        private readonly string[] _tooMuchPlayers = { "player4", "player5", "player6",
-                                                      "player7", "player8", "player9",
-                                                      "player10", "player11" };
+        private const string _tooLessPlayers = "player1,player2";
+        private const string _tooMuchPlayers = "player1,player2,player3,player4,player5," +
+                                               "player6,player7,player8,player9,player10,player11";
 
         private readonly SaboteurGame _game;
         private readonly SaboteurGame _gameWithoutDeadlocks;
@@ -32,6 +32,18 @@ namespace SaboteurTest
             Assert.IsFalse(_game.WithoutDeadlocks, "Simple game failed.");
             Assert.IsTrue(_gameWithoutDeadlocks.WithoutDeadlocks, "WithoutDeadlocks game failed.");
             Assert.IsFalse(_gameSkipLoosers.WithoutDeadlocks, "SkipLoosers game failed.");
+        }
+
+        [DataTestMethod]
+        [DataRow(_tooLessPlayers)]
+        [DataRow(_tooMuchPlayers)]
+        public void OutOfAllowedPlayersCount(string players)
+        {
+            var splittedPlayers = players.Split(',');
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => SaboteurGame.NewGame(false, false, splittedPlayers),
+                $"Players count - {splittedPlayers.Length}"
+            );
         }
     }
 }
