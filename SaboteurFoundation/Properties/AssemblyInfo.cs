1de81c5c839702dfcf649f2c4525e50b692be35b
Vladimir Ivanov <zlumyo@gmail.com>
2018-01-05 13:25:45 +0300

    Made internals visible to unittest project

---
+++ SaboteurFoundation/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для COM-компонентов.  Если необходим доступ к типу в этой сборке из 
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+[assembly: InternalsVisibleTo("SaboteurTest")]
