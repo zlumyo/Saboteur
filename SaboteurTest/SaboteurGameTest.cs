9055bea7e053055083eb21081598b7d5f14c8558
Vladimir Ivanov <zlumyo@gmail.com>
2018-01-05 10:47:54 +0300

    Added unit-test project

---
+++ SaboteurTest/SaboteurGameTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SaboteurTest
+{
+    [TestClass]
+    public class SaboteurGameTest
+    {
+        [TestMethod]
+        public void NewGameTest()
+        {
+        }
+    }
+}
