7e8219ee8153761da1030cd4f49d6bcf84a4c2f2
Vladimir Ivanov <zlumyo@gmail.com>
2018-01-07 19:38:07 +0300

    Added action for HealAlternativeCard

---
+++ SaboteurFoundation/Turn/PlayBufAlternativeAction.cs
@@ -0,0 +1,14 @@
+﻿using SaboteurFoundation.Cards;
+
+namespace SaboteurFoundation.Turn
+{
+    public class PlayBufAlternativeAction : TurnAction
+    {
+        public Player PlayerToBuf { get; }
+
+        public PlayBufAlternativeAction(HealAlternativeCard card, Player player) : base(card)
+        {
+            PlayerToBuf = player;
+        }
+    }
+}
