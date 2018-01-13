bcb25c2de7c9e6402663a0df8e79231b76487c96
Vladimir Ivanov <zlumyo@gmail.com>
2018-01-13 20:23:26 +0300

    Added class for build action

---
+++ SaboteurFoundation/Turn/BuildAction.cs
@@ -0,0 +1,16 @@
+﻿using SaboteurFoundation.Cards;
+
+namespace SaboteurFoundation.Turn
+{
+    public class BuildAction : TurnAction
+    {
+        public TunnelCard NearCard { get; }
+        public ConnectorType SideOfNearCard { get; }
+
+        public BuildAction(TunnelCard card, TunnelCard nearCard, ConnectorType sideOfNearCard) : base(card)
+        {
+            NearCard = nearCard;
+            SideOfNearCard = sideOfNearCard;
+        }
+    }
+}
